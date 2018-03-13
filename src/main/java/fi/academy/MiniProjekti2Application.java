207a2ae2ac41ce39cac01df92a9b0b2652dc27e6
PaulaSazonov <paula.sazonov@gmail.com>
2018-03-13 11:28:29 +0200

    Pohja tehty

---
+++ src/main/java/fi/academy/MiniProjekti2Application.java
@@ -0,0 +1,12 @@
+package fi.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjekti2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjekti2Application.class, args);
+	}
+}
