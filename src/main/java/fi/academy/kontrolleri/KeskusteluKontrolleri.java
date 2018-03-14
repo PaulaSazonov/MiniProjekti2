0a817325207da45e6af4cc3df6a37c203a4139e5
taappana <herranen.jani@gmail.com>
2018-03-14 17:32:02 +0200

    Fiksailtu mergen tulosta

--- src/main/java/fi/academy/kontrolleri/KeskusteluKontrolleri.java
+++ src/main/java/fi/academy/kontrolleri/KeskusteluKontrolleri.java
@@ -21,43 +21,12 @@ public class KeskusteluKontrolleri {
 
     private KeskusteluRepo keskusteluRepo;
     private AiheRepo aiheRepo;
-<<<<<<< Updated upstream
-    private ViestiRepo viestirepo;
-
-    public KeskusteluKontrolleri(@Autowired KeskusteluRepo keskusteluRepo, @Autowired AiheRepo aiheRepo, @Autowired ViestiRepo viestirepo) {
-        this.keskusteluRepo = keskusteluRepo;
-        this.aiheRepo = aiheRepo;
-        this.viestirepo = viestirepo;
-=======
     private ViestiRepo viestiRepo;
 
     public KeskusteluKontrolleri(@Autowired KeskusteluRepo keskusteluRepo, @Autowired AiheRepo aiheRepo, @Autowired ViestiRepo viestiRepo) {
         this.keskusteluRepo = keskusteluRepo;
         this.aiheRepo = aiheRepo;
         this.viestiRepo = viestiRepo;
->>>>>>> Stashed changes
-    }
-
-    @GetMapping("/foorumi/{aiheenNimi}/{id}")
-<<<<<<< Updated upstream
-    public String naytaYksiKeskustelu(@PathVariable("aiheenNimi") String aiheenNimi, @PathVariable("id") int id, Model model) {
-        Optional<Keskustelu> optkesk = keskusteluRepo.findById(id);
-        model.addAttribute("keskustelu", optkesk);
-=======
-    public String naytaYksiKeskustelu (@PathVariable ("aiheenNimi") String aiheenNimi, @PathVariable ("id") int id, Model model){
-        List<Keskustelu> keskustelut = keskusteluRepo.haeKeskustelutAiheella(aiheenNimi);
-        List<Viesti> listaaViestit = viestiRepo.listaaViestit(id);
-        model.addAttribute("keskustelut", keskustelut);
-        model.addAttribute("listaaviestit", listaaViestit);
-        return "keskustelut";
->>>>>>> Stashed changes
-
-        //Lomakkeen luonti
-        Viesti uusiViesti = new Viesti();
-        uusiViesti.setKeskusteluJohonViestiKuuluu(optkesk.get());
-        model.addAttribute("lomake", uusiViesti);
-
-        return "yksiKeskustelu";
     }
 
 //    @GetMapping("/foorumi/haku")
@@ -80,10 +49,35 @@ public class KeskusteluKontrolleri {
 //        this.hakusana = hakusana;
 //    }
 
-    @PostMapping("/uusiviesti")
-    @Transactional
-    public String uudenViestinKasittelija(Viesti viesti) {
-        viestirepo.save(viesti);
-        return "redirect:foorumi"; // halutaan lopulta, että että palaa samaan keskusteluun - nyt ei toimi!
+//        @PostMapping("/uusiviesti")
+//        @Transactional
+//        public String uudenViestinKasittelija (Viesti viesti){
+//            viestirepo.save(viesti);
+//            return "redirect:foorumi"; // halutaan lopulta, että että palaa samaan keskusteluun - nyt ei toimi!
+//        }
+//    }
+
+    @GetMapping("/foorumi/{aiheenNimi}/{id}")
+    public String naytaYksiKeskustelu(@PathVariable("aiheenNimi") String aiheenNimi, @PathVariable("id") int id, Model model) {
+        List<Keskustelu> keskustelut = keskusteluRepo.haeKeskustelutAiheella(aiheenNimi);
+        List<Viesti> listaaViestit = viestiRepo.listaaViestit(id);
+        model.addAttribute("keskustelut", keskustelut);
+        model.addAttribute("listaaviestit", listaaViestit);
+        return "keskustelut";
     }
+
+    // pitäis kattoo, sillä nämä kaks pitäisi yhdistää
+
+//    @GetMapping("/foorumi/{aiheenNimi}/{id}")
+//    public String naytaYksiKeskustelu(@PathVariable("aiheenNimi") String aiheenNimi, @PathVariable("id") int id, Model model) {
+//        Optional<Keskustelu> optkesk = keskusteluRepo.findById(id);
+//        model.addAttribute("keskustelu", optkesk);
+//
+//        //Lomakkeen luonti
+//        Viesti uusiViesti = new Viesti();
+//        uusiViesti.setKeskusteluJohonViestiKuuluu(optkesk.get());
+//        model.addAttribute("lomake", uusiViesti);
+//
+//        return "yksiKeskustelu";
+//    }
 }
