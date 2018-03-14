412504964b4a4248b114e09434c9d29bcc6301fe
PaulaSazonov <paula.sazonov@gmail.com>
2018-03-14 16:57:09 +0200

    Hakutoiminnallisuutta aloitettu

--- src/main/java/fi/academy/repositoryt/ViestiRepo.java
+++ src/main/java/fi/academy/repositoryt/ViestiRepo.java
@@ -3,5 +3,8 @@ package fi.academy.repositoryt;
 import fi.academy.entityt.Viesti;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ViestiRepo extends CrudRepository<Viesti, Integer> {
+    List<Viesti> findViestiByTekstiContaining(String hakusana);
 }
