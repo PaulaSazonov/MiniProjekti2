295af021a6ff82251dd70ed6dcba868210fddcd7
veli-pekka.nurmi <nurmi.vp@gmail.com>
2018-03-15 15:53:20 +0200

    serverside validoinnit OK, paitsi uuden keskustelun otsikon palautus virhetilanteessa

--- src/main/java/fi/academy/kontrolleri/AiheKontrolleri.java
+++ src/main/java/fi/academy/kontrolleri/AiheKontrolleri.java
@@ -57,15 +57,21 @@ public class AiheKontrolleri {
     }
 
     @GetMapping("foorumi/{aiheenNimi}/uusikeskustelu")
-    public String lisaaUusiKeskustelu(@PathVariable String aiheenNimi, Model model) {
+    public String lisaaUusiKeskustelu(@PathVariable String aiheenNimi, Model model, Viesti viesti, Keskustelu keskustelu) {
 
         Optional<Aihe> aihe = aiheRepo.findById(aiheenNimi);
 
         Keskustelu uusiKeskustelu = new Keskustelu();
         uusiKeskustelu.setAihealueJohonKuuluu(aihe.get());
+//        if (keskustelu != null) {// jos palataan virheilmoituksen kautta
+//            uusiKeskustelu = keskustelu;
+//        }
 
         Viesti uusiAloitusviesti = new Viesti();
         uusiAloitusviesti.setKeskusteluJohonViestiKuuluu(uusiKeskustelu);
+        if (viesti != null) { // jos palataan virheilmoituksen kautta
+            uusiAloitusviesti = viesti;
+        }
 
         uusiKeskustelu.setAloitusviesti(uusiAloitusviesti);
 
@@ -86,26 +92,27 @@ public class AiheKontrolleri {
         // TARKISTATAAN ONKO VIESTIN KENTÄT TYHJÄT
         Viesti viesti = keskustelu.getAloitusviesti();
 
-        // TAKISTA KAIKKI KENTÄT
-        if (!viesti.getKirjoittaja().equals("") || !viesti.getTeksti().equals("") || !keskustelu.getKeskustelunotsikko().equals("")) {
-
+        // TARKISTA KAIKKI KENTÄT
+        if (viesti.getKirjoittaja().isEmpty() || viesti.getTeksti().isEmpty() || keskustelu.getKeskustelunotsikko().isEmpty()) {
+            model.addAttribute("virhe", "Täytäthän kaikki pakolliset tiedot");
+            return lisaaUusiKeskustelu(aiheenNimi, model, viesti, keskustelu);
+        } else {
             if (viesti.getTeksti().length() < 2000) {
                 keskusteluRepo.save(keskustelu);
                 int id = keskustelu.getId();
-                return naytaYksiKeskustelu(aiheenNimi, id, model); // pysyy keskustelun sivulla mihin lisäsi viestin
+
+                viesti = null;
+                return naytaYksiKeskustelu(aiheenNimi, id, model, viesti); // pysyy keskustelun sivulla mihin lisäsi viestin
             } else {
                 model.addAttribute("virhe", "Viestin maksimipituus on 2000 merkkiä");
-                return lisaaUusiKeskustelu(aiheenNimi, model);
+                return lisaaUusiKeskustelu(aiheenNimi, model, viesti, keskustelu);
             }
-        } else {
-            model.addAttribute("virhe", "Täytäthän kaikki pakolliset tiedot");
-            return lisaaUusiKeskustelu(aiheenNimi, model);
         }
         //return naytaYksiKeskustelu(aiheenNimi, id, model); // pysyy keskustelun sivulla mihin lisäsi viestin
     }
 
     @GetMapping("/foorumi/{aiheenNimi}/{id}")
-    public String naytaYksiKeskustelu(@PathVariable("aiheenNimi") String aiheenNimi, @PathVariable("id") int id, Model model) {
+    public String naytaYksiKeskustelu(@PathVariable("aiheenNimi") String aiheenNimi, @PathVariable("id") int id, Model model, Viesti viesti) {
         List<Keskustelu> keskustelut = keskusteluRepo.haeKeskustelutAiheella(aiheenNimi);
         if (keskustelut.isEmpty()) {
             return "eiHakutuloksia";
@@ -120,7 +127,12 @@ public class AiheKontrolleri {
         model.addAttribute("listaaviestit", listaaViestit);
 
         Optional<Keskustelu> optkesk = keskusteluRepo.findById(id);
+
         Viesti uusiViesti = new Viesti();
+        if (viesti != null) { // jos palataan virheilmoituksen kautta
+            uusiViesti = viesti;
+        }
+
         uusiViesti.setKeskusteluJohonViestiKuuluu(optkesk.get());
         model.addAttribute("lomake", uusiViesti);
 
@@ -137,13 +149,15 @@ public class AiheKontrolleri {
         int id = viesti.getKeskusteluJohonViestiKuuluu().getId();
 
         // tarkistetaan onko viestin kentät tyhjiä
-        if (!viesti.getKirjoittaja().equals("") || !viesti.getTeksti().equals("")) {
-            Viesti talletettuViesti = viestiRepo.save(viesti);
-        } else {
+        if (viesti.getKirjoittaja().equals("") || viesti.getTeksti().equals("")) {
             model.addAttribute("virhe", "Täytäthän kaikki tarvittavat tiedot!");
+            return naytaYksiKeskustelu(aiheenNimi, id, model, viesti);
+        } else {
+            Viesti talletettuViesti = viestiRepo.save(viesti);
+            viesti = null;
+            return naytaYksiKeskustelu(aiheenNimi, id, model, viesti); // pysyy keskustelun sivulla mihin lisäsi viestin
         }
 
-        return naytaYksiKeskustelu(aiheenNimi, id, model); // pysyy keskustelun sivulla mihin lisäsi viestin
     }
 }
 
